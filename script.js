090491f344a7cec8a216d5bfa1a42549717ae296
JGitSLice <cyberheroja25@gmail.com>
2026-05-31 01:22:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,112 @@
+const subscribeForm = document.getElementById("subscribeForm");
+const eventForm = document.getElementById("eventForm");
+
+const eventsContainer = document.getElementById("eventsContainer");
+
+
+
+async function loadEvents(){
+
+    const response = await fetch("events.json");
+    const events = await response.json();
+
+    eventsContainer.innerHTML = "";
+
+    events.forEach(event => {
+
+        eventsContainer.innerHTML += `
+            <div class="event">
+                <h3>${event.title}</h3>
+                <p class="event-date">${event.date}</p>
+                <p>${event.description}</p>
+            </div>
+        `;
+    });
+}
+
+loadEvents();
+
+
+
+subscribeForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    try{
+
+        await fetch(
+            "YOUR_API_GATEWAY_URL/subscribe",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    email:email
+                })
+            }
+        );
+
+        document.getElementById("subscribeMessage").textContent =
+            "Subscription request sent! Check your email.";
+
+        subscribeForm.reset();
+
+    }catch(error){
+
+        document.getElementById("subscribeMessage").textContent =
+            "Error subscribing.";
+
+    }
+
+});
+
+
+
+eventForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const title =
+        document.getElementById("eventTitle").value;
+
+    const date =
+        document.getElementById("eventDate").value;
+
+    const description =
+        document.getElementById("eventDescription").value;
+
+    try{
+
+        await fetch(
+            "YOUR_API_GATEWAY_URL/create-event",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    title,
+                    date,
+                    description
+                })
+            }
+        );
+
+        document.getElementById("eventMessage").textContent =
+            "Event created successfully.";
+
+        eventForm.reset();
+
+        loadEvents();
+
+    }catch(error){
+
+        document.getElementById("eventMessage").textContent =
+            "Error creating event.";
+
+    }
+
+});
